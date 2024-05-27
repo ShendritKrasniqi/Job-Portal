ad90d0ded47dfa2c13b296a67faf975d50e4cc0d
ShendritKrasniqi <shendritkrasniqi02@gmail.com>
2024-05-28 01:26:45 +0200

    Working with Home Page Part-1

---
+++ app/Models/Job/Job.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\Models\Job;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Job extends Model
+{
+    use HasFactory;
+
+    protected $tabel = 'jobs';
+
+    protected $fillabel = [
+
+    'id',
+    'job_title',
+    'job_region',
+    'job_type',
+    'vacancy',
+    'experience',
+    'salary',
+    'Gender',
+    'application_deadline',
+    'jobdescription',
+    'responsibilities',
+    'education_experience',
+    'otherbenifits',
+    'image',
+
+    ];
+
+    public $timestamps = true;
+}
