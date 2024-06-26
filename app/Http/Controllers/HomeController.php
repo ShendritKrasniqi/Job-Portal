409fa5b0e574301d1f0d283022bc69123854933e
ShendritKrasniqi <shendritkrasniqi02@gmail.com>
2024-06-26 17:00:00 +0200

    Working with Trending Keywords Part-2

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Models\Job\Job;
+use DB;
 
 class HomeController extends Controller
 {
@@ -24,10 +25,19 @@ class HomeController extends Controller
      */
     public function index()
     {
+
+        $duplicates = DB::table('searches')
+        ->select('keyword', DB::raw("COUNT(*) as 'count'"))
+        ->groupBy('keyword')
+        ->havingRaw('COUNT(*) > 1')
+        ->take(3)
+        ->orderBy('count', 'asc')
+        ->get();
+
         $jobs = Job::select()->take(5)->orderby('id', 'desc')->get();
         $totalJobs= Job::all()->count();
 
-        return view('home', compact('jobs', 'totalJobs'));
+        return view('home', compact('jobs', 'totalJobs', 'duplicates'));
     }
 
     public function about()
