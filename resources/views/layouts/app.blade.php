b54fd6b78b0ef0210bc4682bdc296fa7e906c143
ShendritKrasniqi <shendritkrasniqi02@gmail.com>
2024-10-25 01:30:12 +0200

    Update Navbar and Fix Hero Section on All Pages

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -38,69 +38,70 @@
         <header class="site-navbar mt-3">
       <div class="container-fluid">
         <div class="row align-items-center">
-          <div class="site-logo col-6"><a href="{{ url('/') }}">JobBoard</a></div>
+        <div class="site-logo col-6"><a href="{{ url('/') }}">JobBoard</a></div>
 
           <nav class="mx-auto site-navigation">
-            <ul class="site-menu js-clone-nav d-none d-xl-block ml-0 pl-0">
-              <li style="padding-left: 544px;"><a href="{{ url('/') }}" class="nav-link active">Home</a></li>
-              <li><a href="{{ route('about') }}">About</a></li>
-                          
-              <li><a href="{{ route('contact') }}">Contact</a></li>
-              <li class="d-lg-none"><a href="post-job.html"><span class="mr-2">+</span> Post a Job</a></li>
-              
-              @guest
-                @if (Route::has('login'))
-              <li class=""><a href="{{ route('login') }}">Log In</a></li>
-              @endif
+    <ul class="site-menu js-clone-nav d-none d-xl-block ml-0 pl-0">
+        <li style="padding-left: 544px;">
+            <a href="{{ url('/') }}" class="nav-link {{ request()->is('/') ? 'active' : '' }}">Home</a>
+        </li>
+        <li>
+            <a href="{{ route('about') }}" class="nav-link {{ request()->is('about') ? 'active' : '' }}">About</a>
+        </li>
+        <li>
+            <a href="{{ route('contact') }}" class="nav-link {{ request()->is('contact') ? 'active' : '' }}">Contact</a>
+        </li>
+        <li class="d-lg-none">
+            <a href="post-job.html"><span class="mr-2">+</span> Post a Job</a>
+        </li>
 
-              @if (Route::has('register'))
-              <li class=""><a href="{{ route('register') }}">Register</a></li>
-              @endif
+        @guest
+            @if (Route::has('login'))
+            <li>
+                <a href="{{ route('login') }}" class="nav-link {{ request()->is('login') ? 'active' : '' }}">Log In</a>
+            </li>
+            @endif
 
-              @else
-              <li class="nav-item dropdown">
-                                <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
-                                    {{ Auth::user()->name }}
-                                </a>
+            @if (Route::has('register'))
+            <li>
+                <a href="{{ route('register') }}" class="nav-link {{ request()->is('register') ? 'active' : '' }}">Register</a>
+            </li>
+            @endif
+        @else
+        <li class="nav-item dropdown">
+            <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
+                {{ Auth::user()->name }}
+            </a>
 
-                                <div class="dropdown-menu dropdown-menu-end" aria-labelledby="navbarDropdown">
+            <div class="dropdown-menu dropdown-menu-end" aria-labelledby="navbarDropdown">
+                <a class="dropdown-item" href="{{ route('profile') }}">
+                    Profile
+                </a>
+                <a class="dropdown-item" href="{{ route('saved.jobs') }}">
+                    Saved Jobs
+                </a>
+                <a class="dropdown-item" href="{{ route('edit.details') }}">
+                    Update Details
+                </a>
+                <a class="dropdown-item" href="{{ route('edit.cv') }}">
+                    Update CV
+                </a>
+                <a class="dropdown-item" href="{{ route('applications') }}">
+                    Applications
+                </a>
+                <a class="dropdown-item" href="{{ route('logout') }}"
+                   onclick="event.preventDefault(); document.getElementById('logout-form').submit();">
+                    {{ __('Logout') }}
+                </a>
 
-                                    <a class="dropdown-item" href="{{ route('profile') }}">
-                                        Profile
-                                    </a>
-
-                                    <a class="dropdown-item" href="{{ route('saved.jobs') }}">
-                                        Saved Jobs
-                                    </a>
-
-                                    <a class="dropdown-item" href="{{ route('edit.details') }}">
-                                        Update Details
-                                    </a>
-
-                                    <a class="dropdown-item" href="{{ route('edit.cv') }}">
-                                        Update CV
-                                    </a>
-
-                                    <a class="dropdown-item" href="{{ route('applications') }}">
-                                        Applications
-                                    </a>
-
-                                    <a class="dropdown-item" href="{{ route('logout') }}"
-                                       onclick="event.preventDefault();
-                                                     document.getElementById('logout-form').submit();">
-                                        {{ __('Logout') }}
-                                    </a>
-
-
-                                    <form id="logout-form" action="{{ route('logout') }}" method="POST" class="d-none">
-                                        @csrf
-                                    </form>
-                                </div>
-                            </li>
-                 @endguest
-
-            </ul>
-          </nav>
+                <form id="logout-form" action="{{ route('logout') }}" method="POST" class="d-none">
+                    @csrf
+                </form>
+            </div>
+        </li>
+        @endguest
+    </ul>
+</nav>
           
           <div class="right-cta-menu text-right d-flex aligin-items-center col-6">
             <a href="#" class="site-menu-toggle js-menu-toggle d-inline-block d-xl-none mt-lg-2 ml-3"><span class="icon-menu h3 m-0 p-0 mt-2"></span></a>
